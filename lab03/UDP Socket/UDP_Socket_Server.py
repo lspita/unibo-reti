5d50cba4c4fa3516567b4872fd5bf56d35a20866
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-03-18 15:09:38 +0100

    auto format

--- lab03/UDP Socket/UDP_Socket_Server.py
+++ lab03/UDP Socket/UDP_Socket_Server.py
@@ -1,8 +1,8 @@
-'''
+"""
                             UDP SERVER SOCKET
 Corso di Programmazione di Reti - Laboratorio - Università di Bologna
 G.Pau - A. Piroddi
-'''
+"""
 
 import socket as sk
 import time
@@ -11,21 +11,19 @@ import time
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 
 # associamo il socket alla porta
-server_address = ('localhost', 10000)
-print ('\n\r starting up on %s port %s' % server_address)
+server_address = ("localhost", 10000)
+print("\n\r starting up on %s port %s" % server_address)
 sock.bind(server_address)
 
 while True:
-    print('\n\r waiting to receive message...')
+    print("\n\r waiting to receive message...")
     data, address = sock.recvfrom(4096)
 
-    print('received %s bytes from %s' % (len(data), address))
-    print (data.decode('utf8'))
-    
-    
+    print("received %s bytes from %s" % (len(data), address))
+    print(data.decode("utf8"))
+
     if data:
-        data1='Programmazione di Reti'
+        data1 = "Programmazione di Reti"
         time.sleep(2)
         sent = sock.sendto(data1.encode(), address)
-        print ('sent %s bytes back to %s' % (sent, address))
-
+        print("sent %s bytes back to %s" % (sent, address))
